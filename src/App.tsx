a7c3c9280ffe08805ff88eb96c0466db42467813
郝力衝 <wb-hlc336023@antfin.com>
2019-07-24 11:38:47 +0800

    dev

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BaseLayout from './components/BaseLayout';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <BaseLayout />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+const IndexPage = () => {
+  return <Redirect to="/BizSelect" />;
+};
+
+export default IndexPage;
