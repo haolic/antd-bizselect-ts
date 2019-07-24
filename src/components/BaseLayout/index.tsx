e29e094a4798d9dc2e33ffc33d30da02da5e7745
郝力衝 <wb-hlc336023@antfin.com>
2019-07-24 14:12:35 +0800

    router更改

--- src/components/BaseLayout/index.tsx
+++ src/components/BaseLayout/index.tsx
@@ -1,51 +1,48 @@
 import React, { useState } from 'react';
 import { Layout, Menu, Icon } from 'antd';
+import { Link, BrowserRouter } from 'react-router-dom';
 
 const { Header, Sider, Content } = Layout;
 
 const BaseLayout = (props: any) => {
   const [collapsed, setCollapsed] = useState(false);
   return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">
-            <Icon type="user" />
-            <span>nav 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="video-camera" />
-            <span>nav 2</span>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="upload" />
-            <span>nav 3</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
+    <BrowserRouter>
       <Layout>
-        <Header style={{ background: '#fff', padding: 0 }}>
-          <Icon
-            className="trigger"
-            type={collapsed ? 'menu-unfold' : 'menu-fold'}
-            onClick={() => {
-              setCollapsed(!collapsed);
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Link to="/BizSelect">
+                <Icon type="user" />
+                <span>BizSelect</span>
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={() => {
+                setCollapsed(!collapsed);
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 280
             }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            background: '#fff',
-            minHeight: 280
-          }}
-        >
-          {props.children}
-        </Content>
+          >
+            {props.children}
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </BrowserRouter>
   );
 };
 
