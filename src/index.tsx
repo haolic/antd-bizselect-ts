31bac9d9666014dd47c5892eb42b19bc5f93e233
郝力衝 <wb-hlc336023@antfin.com>
2019-09-20 17:30:08 +0800

    dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import { renderToString } from 'react-dom/server';
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+// console.log(renderToString(<App />))
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
